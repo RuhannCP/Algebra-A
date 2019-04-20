b7b763354766c10de99022c894656e4e187bd2b6
EwertonDCSilv <ewertonsilva@dcc.ufmg.br>
2019-04-20 12:11:15 -0300

    Error mpz_init

--- main.c
+++ main.c
@@ -6,10 +6,24 @@
 int main(int argc, char * argv[]){
 	int n = 0;
 	mpz_t a,b,x,y,g;
+
+	mpz_init(a);
+	mpz_init(b);
+	mpz_init(x); 
+	mpz_init(y); 
+	mpz_init(g); 
+	
 	gmp_scanf("%Z %Z %Z %Z",&a,&b,&x,&y);
-	//mpz_inits ( g,x,y,a,b);
+
+	gmp_printf("%Z",a);
 
 	//mpz_mdc(g,x,y,a,b);
 	
+	mpz_clear(a);
+	mpz_clear(b);
+	mpz_clear(x); 
+	mpz_clear(y); 
+	mpz_clear(g); 
+
 	return 1;
 }
