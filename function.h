b333171618c8469cbfc6fc636370738a0c22538d
RuhannCP <ruhannc@ufmg.br>
2019-04-23 14:00:58 -0300

    exp-binaria

--- function.h
+++ function.h
@@ -4,5 +4,5 @@
 
 void mdc_estendido(mpz_t g, mpz_t x, mpz_t y, const mpz_t a, const mpz_t b);
 int inverso_modular(mpz_t r, const mpz_t a, const mpz_t n);
-//void exp_binaria(mpz_t r,const mpz_t b,const mpz_t e,const mpz_t n);
-//void fact(int n);
+void exp_binaria(mpz_t r,const mpz_t b,const mpz_t e,const mpz_t n);
+void binary_exp(mpz_t r, const mpz_t b, const mpz_t e, const mpz_t n);
--- main.c
+++ main.c
@@ -5,31 +5,20 @@
 #include "function.h"
 
 int main(int argc, char * argv[]){
-	mpz_t a;
-	mpz_init (a);
-	mpz_t b;
-	mpz_init (b);
-	mpz_t x;
-	mpz_init (x);
-	mpz_t y;
-	mpz_init (y);
-	mpz_t g;
-	mpz_init (g);
-	mpz_t r;
-	mpz_init(r);
+	mpz_t a,b,x,y,g,r;
+	mpz_inits (a,b,x,y,g,r,NULL);
 	
 	gmp_scanf("%Zd",&a);
 	gmp_scanf("%Zd",&b);
+	gmp_scanf("%Zd",&x);
 
-	gmp_printf("A: %Zd \n\n",a);
-	gmp_printf("B: %Zd \n\n",b);
+	binary_exp(r,a,b,x);
+	exp_binaria(g,a,b,x);
 	
-	mdc_estendido(g,x,y,a,b);	
-	gmp_printf("MDC: %Zd \n\n",g);
+	gmp_printf("\n exp-bin r = %Zd\n",r);
+	gmp_printf("\n exp-bin g = %Zd\n",g);
 
-	inverso_modular(r,a,b);
-	gmp_printf("\ninverso = %Zd\n",r);
-	mpz_clears (a,b,r,NULL);
+	mpz_clears (a,b,r,y,g,x,NULL);
 	
 	return 0;
 }
