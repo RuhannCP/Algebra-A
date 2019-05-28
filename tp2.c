6856ba38f3aee0a1ca41c032eadf65454e5da7be
RuhannCP <ruhannc@ufmg.br>
2019-05-28 09:41:08 -0300

    trabalho pronto e testado

--- tp2.c
+++ tp2.c
@@ -1,86 +1,177 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gmp.h>
-#include <math.h>
-#include "function.h"
 
-void numero_aleatorio(mpz_t r, const mpz_t n, gmp_randstate_t rnd) {//Função dada
-    mp_bitcnt_t num_bits = mpz_sizeinbase(n, 2);
-    
-    do {
-            mpz_urandomb(r, rnd, num_bits);
-        } while (!(mpz_cmp_ui(r, 1) >= 0 && mpz_cmp(r, n) <= 0));
-    }
+void mdc_estendido(mpz_t g, mpz_t x, mpz_t y, const mpz_t a, const mpz_t b){
 
-int talvez_primo(const mpz_t a, const mpz_t n, const mpz_t n1, unsigned int t, const mpz_t q){
-    int k=0,i=0;
-    mpz_t aux,aux1;
-    mpz_inits (aux, aux1, NULL);
-    mpz_div_ui (aux,n,2);
-    mpz_div_ui (aux1,n,5);
-    
-    if (mpz_cmp_ui(aux,0) || mpz_cmp_ui(aux1,0)){//Para caso n seja par ou multiplo de cinco
-        return 0;
-    }
-    for (int k=0;k<=n1;k++){// definindo (n-1) com fatores de q*2k
-        t = pow(2,k);
-        mpz_div_ui (aux1,n1,t);
-        mpz_mod_ui (aux,aux1,2);
-        if (!mpz_cmp_ui(aux,0)){
-            break;
-        }
-    }
-    mpz_set(q,aux1);//definindo o valor de q
-    mpz_pow(aux,a,q);
-    mpz_mod (aux,aux,n);
-    if (!mpz_cmp(aux,1)){//caso valor seja diferente de um esse numero é composto
-        return 0;
-    }
-    for (i = 1;i <= k;i++){//verificando se existe algum i tal que 2î*q seja congruente a 1 mod(p)
-        t = pow(2,i);
-        mpz_mul_ui(aux,q,t);
-        mpz_mod (aux,aux,n);
-        if (mpz_cmp_ui(aux,1) && i > 1){
-            return 0;
-        }
-    }
-    /*-----retorno da função composto ou pode ser primo -------*/
-    if (mpz_cmp(aux,n1) || mpz_cmp_ui(aux,1)){
-        return 1;
-    }else{
-        return 0;
-    }
-    mpz_clears (aux, aux1, NULL);
+	mpz_t resto, auxX, auxY, quociente, aux;
+	mpz_inits(resto, auxX, auxY, quociente, aux, NULL);
+	mpz_set(auxX , x);
+	mpz_set(auxY , y);
+	
+	if(mpz_cmp_ui(b, 0)==0){
+		mpz_set_ui(x, 1);
+		mpz_set_ui(y, 0);
+		mpz_set(g,a);
+		return;
+	}
+
+	mpz_fdiv_qr(quociente, resto, a, b);
+	mdc_estendido(g, auxX, auxY, b, resto);
+	
+	mpz_set(x, auxY);
+	mpz_mul(aux, quociente, auxY);
+	mpz_sub(y, auxX, aux);
+
+	mpz_clears(resto, auxX, auxY, quociente, aux, NULL);
+}
+
+int inverso_modular(mpz_t r, const mpz_t a, const mpz_t n){
+	mpz_t g, x, y;
+	mpz_inits(g, x, y, NULL);
+
+	mdc_estendido(g, x, y, n, a);
+
+	if (mpz_cmp_ui(g, 1)==0){
+		mpz_mod(r, y, n);
+		return 1;
+	}
+	
+	mpz_clears(g, x, y, NULL);	
+
+	return 0;
+}
+
+void exp_binaria(mpz_t r, const mpz_t b, const mpz_t e, const mpz_t n){
+	
+	mpz_t aux;
+	mpz_init(aux);
+
+	if(mpz_cmp_ui (e, 0) == 0){
+		mpz_set_ui(r, 1);
+		return;
+	}
+
+	else {
+		mpz_fdiv_q_ui(aux, e, 2);
+		exp_binaria(r, b, aux, n);
+		mpz_mul(aux, r, r);
+		mpz_mod(aux, aux, n);
+		mpz_set(r, aux);
+		mpz_mod_ui(aux, e, 2);
+		if(mpz_cmp_ui (aux, 1) == 0){
+			mpz_mul(aux, r, b);
+			mpz_mod(aux, aux, n);
+			mpz_set(r, aux);
+		}
+  	}
+
+	mpz_clear(aux);
+}
+
+void calcula_tqn(const mpz_t n, mpz_t n1, unsigned int *t, mpz_t q){
+
+	*t=0;
+	mpz_t aux, auxN1;
+	mpz_inits(aux, auxN1, NULL);
+	mpz_sub_ui(aux, n, 1);
+	mpz_set(n1, aux);
+	mpz_set(auxN1, n1);
+	
+	mpz_mod_ui(aux, auxN1,2);
+	while(mpz_cmp_ui(aux,0)==0){
+		mpz_fdiv_q_ui(auxN1, auxN1, 2);
+		*t += 1;
+		mpz_mod_ui(aux, auxN1,2);
+	}
+
+	mpz_set(q, auxN1);
+	
+	mpz_clears(aux, auxN1, NULL);
+}
+
+int talvez_primo(const mpz_t a,	const mpz_t n, const mpz_t n1, unsigned int t, const mpz_t q){
+
+	unsigned int i;
+	mpz_t aux, e;
+	mpz_inits(aux, e, NULL);
+	
+	if (mpz_cmp_ui(n,1)==0 || mpz_cmp_ui(n,2)==0)
+		return 1;
+	
+	mpz_mod_ui(aux, n, 2);
+	if (mpz_cmp_ui(aux,0)==0)
+		return 0;
+
+	
+	else{ 
+		
+		exp_binaria(aux, a, q, n);
+		if(mpz_cmp_ui(aux,1)==0 || mpz_cmp(aux, n1)==0)
+			return 1;
+
+		for(i=1; i<t; i++){ 
+			
+			mpz_ui_pow_ui(aux, 2, i);
+			mpz_mul(aux, aux, q);
+			mpz_set(e, aux);
+
+			exp_binaria(aux, a, e, n);
+	
+			if(mpz_cmp(aux, n1)==0)
+				return 1;
+		}
+	}
+
+	mpz_clears(aux, e, NULL);
+}
+
+void random_n(mpz_t r, const mpz_t n, gmp_randstate_t rnd) {
+	
+	mp_bitcnt_t num_bits = mpz_sizeinbase(n, 2);
+	do {
+		mpz_urandomb(r, rnd, num_bits);
+	} while (!(mpz_cmp_ui(r, 1) >= 0 && mpz_cmp(r, n) <= 0));
 }
 
 int provavelmente_primo(const mpz_t n, unsigned int iter, gmp_randstate_t rnd){
-    int i = 0,x=0;
-    mpz_t r,n1,t,q;
-    mpz_inits (r,n1,t,q,NULL);
-    mpz_sub_ui (n1,n,1);
-    for(i=1;i<=iter;i++){//Loop verifica primo
-        numero_aleatorio(r, n,NULL);//Gerador de possiveis "a"
-        
-        x = talvez_primo(r,n,n1,t,q);
-        if(x == 0){
-            return 0;
-        }
-    }
-    mpz_clears(r,n1,t,q,NULL);
-    return 1;
+	mpz_t a, aux, q, n1;
+	unsigned int t, miller;
+	mpz_inits(a, aux, q, n1, NULL);
+	unsigned int i;
+
+	if (mpz_cmp_ui(n,1)==0 || mpz_cmp_ui(n,2)==0)
+		return 1;
+	
+	mpz_mod_ui(aux, n, 2);
+	if (mpz_cmp_ui(aux,0)==0)
+		return 0;
+
+	calcula_tqn(n, n1, &t, q);	
+
+	for(i=0; i<iter; i++){
+
+		mpz_sub_ui(aux, n, 2);
+		random_n(a, aux, rnd);
+		mpz_add_ui(a, a, 1);
+		miller = talvez_primo(a, n, n1, t, q);
+
+		if(miller == 0)
+			return 0;
+
+	}
+
+	mpz_clears(a, aux, q, n1, NULL);
+	return 1;
 }
 
-void primo_aleatorio(mpz_t r, unsigned int b, gmp_randstate_t rnd){
-    int n = pow(2,b);
-    int x = 0;
-    do{
-        numero_aleatorio (r,n,rnd);
+void primo_aleatorio(mpz_t r,unsigned int b,gmp_randstate_t rnd){
 
-        x = provavelmente_primo(r,n,rnd);
-        
-        if (x == 1){
-            return 1;
-        }
-    }while(x == 0);
-    return 0;
+	int provPrimo=0;
+	unsigned int iter = 20;
+
+	while(!provPrimo){
+		mpz_urandomb(r, rnd, b);
+		provPrimo = provavelmente_primo(r, iter, rnd);
+	}
 }
--- tp3.c
+++ tp3.c
@@ -1,62 +1,228 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gmp.h>
-#include <math.h>
-#include "function.h"
+#include <string.h>
+
+
+void exp_binaria(mpz_t r, const mpz_t b, const mpz_t e, const mpz_t n){
+	
+	mpz_t aux;
+	mpz_init(aux);
+
+	if(mpz_cmp_ui (e, 0) == 0){
+		mpz_set_ui(r, 1);
+		return;
+	}
+
+	else {
+		mpz_fdiv_q_ui(aux, e, 2);
+		exp_binaria(r, b, aux, n);
+
+		mpz_mul(aux, r, r);
+		mpz_mod(aux, aux, n);
+		mpz_set(r, aux);
+
+		mpz_mod_ui(aux, e, 2);
+		if(mpz_cmp_ui (aux, 1) == 0){
+			mpz_mul(aux, r, b);
+			mpz_mod(aux, aux, n);
+			mpz_set(r, aux);
+		}
+
+  	}
+
+	mpz_clear(aux);
+}
+
+void mdc_estendido(mpz_t g, mpz_t x, mpz_t y, const mpz_t a, const mpz_t b){
+
+	mpz_t resto, auxX, auxY, quociente, aux;
+	mpz_inits(resto, auxX, auxY, quociente, aux, NULL);
+	mpz_set(auxX , x);
+	mpz_set(auxY , y);
+	
+	if(mpz_cmp_ui(b, 0)==0){
+		mpz_set_ui(x, 1);
+		mpz_set_ui(y, 0);
+		mpz_set(g,a);
+		return;
+	}
+
+	mpz_fdiv_qr(quociente, resto, a, b);
+	mdc_estendido(g, auxX, auxY, b, resto);
+	
+	mpz_set(x, auxY);
+	mpz_mul(aux, quociente, auxY);
+	mpz_sub(y, auxX, aux);
+
+	mpz_clears(resto, auxX, auxY, quociente, aux, NULL);
+}
+
+int inverso_modular(mpz_t r, const mpz_t a, const mpz_t n){
+	mpz_t g, x, y;
+	mpz_inits(g, x, y, NULL);
+
+	mdc_estendido(g, x, y, n, a);
+
+	if (mpz_cmp_ui(g, 1)==0){
+		mpz_mod(r, y, n);
+		return 1;
+	}
+	
+	mpz_clears(g, x, y, NULL);	
+
+	return 0;
+}
+
+
+void calcula_tqn(const mpz_t n, mpz_t n1, unsigned int *t, mpz_t q){
+
+	*t=0;
+	mpz_t aux, auxN1;
+	mpz_inits(aux, auxN1, NULL);
+	mpz_sub_ui(aux, n, 1);
+	mpz_set(n1, aux);
+	mpz_set(auxN1, n1);
+	
+	mpz_mod_ui(aux, auxN1,2);
+	while(mpz_cmp_ui(aux,0)==0){
+		mpz_fdiv_q_ui(auxN1, auxN1, 2);
+		*t += 1;
+		mpz_mod_ui(aux, auxN1,2);
+	}
+
+	mpz_set(q, auxN1);
+	
+	mpz_clears(aux, auxN1, NULL);
+}
+
+int talvez_primo(const mpz_t a,	const mpz_t n, const mpz_t n1, unsigned int t, const mpz_t q){
+
+	unsigned int i;
+	mpz_t aux, e;
+	mpz_inits(aux, e, NULL);
+	
+	if (mpz_cmp_ui(n,1)==0 || mpz_cmp_ui(n,2)==0)
+		return 1;
+	
+	mpz_mod_ui(aux, n, 2);
+	if (mpz_cmp_ui(aux,0)==0)
+		return 0;
+
+	
+	else{
+		
+		exp_binaria(aux, a, q, n);
+		if(mpz_cmp_ui(aux,1)==0 || mpz_cmp(aux, n1)==0)
+			return 1;
+
+		for(i=1; i<t; i++){
+			
+			mpz_ui_pow_ui(aux, 2, i);
+			mpz_mul(aux, aux, q);
+			mpz_set(e, aux);
+
+			exp_binaria(aux, a, e, n);
+	
+			if(mpz_cmp(aux, n1)==0)
+				return 1;
+		}
+	}
+
+	mpz_clears(aux, e, NULL);
+}
+
+void random_n(mpz_t r, const mpz_t n, gmp_randstate_t rnd) {
+	
+	mp_bitcnt_t num_bits = mpz_sizeinbase(n, 2);
+	do {
+		mpz_urandomb(r, rnd, num_bits);
+	} while (!(mpz_cmp_ui(r, 1) >= 0 && mpz_cmp(r, n) <= 0));
+}
+
+int provavelmente_primo(const mpz_t n, unsigned int iter, gmp_randstate_t rnd){
+	mpz_t a, aux, q, n1;
+	unsigned int t, miller;
+	mpz_inits(a, aux, q, n1, NULL);
+	unsigned int i;
+
+	if (mpz_cmp_ui(n,1)==0 || mpz_cmp_ui(n,2)==0)
+		return 1;
+	
+	mpz_mod_ui(aux, n, 2);
+	if (mpz_cmp_ui(aux,0)==0)
+		return 0;
+
+	calcula_tqn(n, n1, &t, q);	
+
+	for(i=0; i<iter; i++){
+		mpz_sub_ui(aux, n, 2);
+		random_n(a, aux, rnd);
+		mpz_add_ui(a, a, 1);
+
+		miller = talvez_primo(a, n, n1, t, q);
+
+		if(miller == 0)
+			return 0;
+
+	}
+
+	mpz_clears(a, aux, q, n1, NULL);
+	return 1;
+}
+
+void primo_aleatorio(mpz_t r,unsigned int b,gmp_randstate_t rnd){
+
+	int provPrimo=0;
+	unsigned int iter = 20;
+
+	while(!provPrimo){
+		mpz_urandomb(r, rnd, b);
+		provPrimo = provavelmente_primo(r, iter, rnd);
+	}
+}
 
 void gera_chaves(mpz_t n, mpz_t e, mpz_t d, gmp_randstate_t rnd){
-    mpz_t p,q,tp,tq,fi,g,y;
+	
+	mpz_t p, q, g, x, y;
+	mpz_inits(p, q, g, x, y, NULL);
 
-    mpz_inits(p,tp,tq,g,y,q,NULL);
+	unsigned int exp=2048;
+	primo_aleatorio(p, exp, rnd);
+	primo_aleatorio(q, exp, rnd);
+	mpz_mul(n, p, q);
 
-    //Generating prime numbers...
-    primo_aleatorio(p,2048,rnd);
-    primo_aleatorio(q,2048,rnd);
+	mpz_set_ui(e, 65537);
+	mdc_estendido(g, x, y, n, e);
+	while(mpz_cmp_ui(g,1)!=0){
+		mdc_estendido(g, x, y, n, e);
+		mpz_add_ui(e, e, 1);
+	}
 
-    mpz_mul (n,p,q);
+	mpz_t phi, aux;
+	mpz_inits(phi, aux, NULL);
+	mpz_sub_ui(aux, p, 1);
+	mpz_set(phi, aux);
+	mpz_sub_ui(aux, q, 1);
+	mpz_mul(phi, phi, aux);
 
-    //Calculate phi(n)....
-    mpz_sub_ui(tp,p,1);
-    mpz_sub_ui(tq,q,1);
-    mpz_mul(fi,tp,tq);
+	inverso_modular(d, e, phi);
 
-    mpz_set_ui(e,65537);
 
-    //calculate keys primary and secondary...
-    do{
-        mpz_add_ui(e,e,1);
-        mdc_estendido( g, d, y, e, fi);
-    }
-    while (mpz_cmp_ui(g,1)!=1);
-
-    mpz_clears(p,tp,tq,g,y,q,NULL);
+	mpz_clears(p, q, g, x, y, phi, aux, NULL);
 }
 
-void codifica(mpz_t r, const char *str){
-    int i, n = strlen(str);
-    mpz_t r1;
-    mpz_init (r1);
+char* decodifica(const mpz_t n){
 
-    //Encodes the message
-    for (i=0;i<=n;i++){
-        mpz_pow_ui (r1,256,i);
-        mpz_mul_ui(r1,r1,str[i]);
-        mpz_add(r,r,r1);
-    }
-    mpz_clear(r1);
-
-}
-
-char *decodifica(const mpz_t n){
-    char *str = (char*) malloc(501*sizeof(char));
-	unsigned int k, letra;
+	char *str = (char*) malloc(501*sizeof(char));
+	unsigned int i, letra;
 
 	mpz_t c, n1, aux;
 	mpz_inits(c, n1, aux, NULL);
 	
 	mpz_set(n1, n);
 
- 	for(k=0; k<500; k++){
+ 	for(i=0; i<500; i++){
 		
 		if(mpz_cmp_ui(n1,0)==0)
 			break;
@@ -78,13 +244,27 @@ char *decodifica(const mpz_t n){
 	mpz_clears(c, n1, aux, NULL);
 
 	return str;
-
 }
 
-void criptografa(mpz_t C,const mpz_t M,const mpz_t n,const mpz_t e){
-	binary_exp(C, M, e, n);
+void codifica(mpz_t r, const char *str){
+	int n = strlen(str);
+	int i;
+	mpz_t aux;
+	mpz_init(aux);
+	char c='a';
+	for(i=0; i<n; i++){
+		mpz_ui_pow_ui(aux, 256, i);
+		mpz_mul_ui(aux, aux, str[i]);
+		mpz_add(r, r, aux);
+	}
+
+	mpz_clear(aux);
 }
 
-void descriptografa(mpz_t M,const mpz_t C,const mpz_t n,const mpz_t d){
-	binary_exp(M, C, d, n);
+void criptografa(mpz_t C, mpz_t M, mpz_t n, mpz_t e){
+	exp_binaria(C, M, e, n);
+}
+
+void descriptografa(mpz_t M, const mpz_t C, const mpz_t n, const mpz_t d){
+	exp_binaria(M, C, d, n);
 }
